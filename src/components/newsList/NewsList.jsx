cffddb0b6ff200d69cc490f0d8239b97904a3670
Egor Osipchuk <osipchuk.egor-osipchuck@yandex.ru>
2025-05-17 18:27:26 +0700

    3-commit

--- src/components/newsList/NewsList.jsx
+++ src/components/newsList/NewsList.jsx
@@ -1,8 +1,7 @@
 import { NewsItem } from '../newsItem/NewsItem'
 import styles from './NewsList.module.css'
 
-// eslint-disable-next-line no-undef
-export default NewsList = ({ news }) => {
+export const NewsList = ({ news }) => {
 	return (
 		<ul className={styles.list}>
 			{news.map(item => (
