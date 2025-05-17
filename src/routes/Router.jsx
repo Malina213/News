4aecdc3e1cf4b8a9c632d3517eed67b41b0283da
Egor Osipchuk <osipchuk.egor-osipchuck@yandex.ru>
2025-05-17 17:25:53 +0700

    2-commit

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,5 +1,14 @@
-import { RouterProvider } from 'react-router'
-import { router } from './routes'
+import { Layout } from '../components/layout/Layout'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { Home } from '../components/page/Home'
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [{ index: true, element: <Home /> }]
+	}
+])
 
 export const Router = () => {
 	return <RouterProvider router={router} />
