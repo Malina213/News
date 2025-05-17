1cf7115285ec880fffee415f7c813c1044bf3979
Egor Osipchuk <osipchuk.egor-osipchuck@yandex.ru>
2025-05-17 18:44:13 +0700

    3-commit

--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -1,6 +1,6 @@
 import { useNews } from '../../utils/hooks/useNews'
 import { NewsBanner } from '../newsBanner/NewsBanner'
-import { NewsList } from '../newsList/newsList'
+// import { NewsList } from '../newsList/newsList'
 
 export const Home = () => {
 	const { news } = useNews()
@@ -8,7 +8,7 @@ export const Home = () => {
 	return (
 		<section>
 			{news?.length > 0 ? <NewsBanner banner={news[0]} /> : null}
-			<NewsList news={news} />
+			{/* <NewsList news={news} /> */}
 		</section>
 	)
 }
