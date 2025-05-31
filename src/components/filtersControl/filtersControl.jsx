006db1aac43e40832457b0188fd9fceb4f644730
Egor Osipchuk <osipchuk.egor-osipchuck@yandex.ru>
2025-05-31 21:22:56 +0700

    refactoring-2-14

--- src/components/filtersControl/filtersControl.jsx
+++ src/components/filtersControl/filtersControl.jsx
@@ -2,7 +2,7 @@ import { getCategories } from '../../api/api'
 import { useFetch } from '../../helpers/hooks/useFetch'
 import { Categories } from '../categories/Categories'
 import { Search } from '../search/Search'
-import styles from './FiltersControl.module.css'
+import styles from './filtersControl.module.css'
 
 export const FiltersControl = ({ filters, changeFilter }) => {
 	const { data: dataCategories } = useFetch(getCategories)
